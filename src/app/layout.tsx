f4c30d1aca5c324cfebfaafce0d93c3e2ff9e4af
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2025-04-25 16:41:02 +0100

    Project setup and init

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { libreFranklin } from '@/styles/fonts'
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Ping coming soon page",
+  description: "A Frontend Mentor challenge project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${libreFranklin.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Logo from '@/components/atoms/Logo';
+
+export default function Home() {
+  return (
+    <div
+      className='
+      flex flex-col
+      justify-center items-center
+      w-full
+      h-screen min-h-screen 
+      p-8 pb-20 gap-16 
+      sm:p-20'
+    >
+      <Logo />
+      We are launching soon! Subscribe and get notified Notify Me &copy;
+      Copyright Ping. All rights reserved.
+      <footer>
+        <p className='attribution'>
+          Challenge by{' '}
+          <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+            Frontend Mentor
+          </a>
+          . Coded by <a href='#'>Your Name Here</a>.
+        </p>
+      </footer>
+    </div>
+  );
+}
