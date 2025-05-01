fadfddd536da7f77e6dd393ca7d220d3363dcacd
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2025-05-01 19:08:59 +0100

    refactor: entire page.tsx component tailwind style utils and the way the jsx is structured. refactor: same refactor treatment to the EmailForm.tsx component. refactor: removed tailwind base layers and moved those styles into the component jsx.
    rational: I started the project from a mobile first approach. All seemed fine
    with my layout on mobile, then I moved to desktop. I noted at this point that
    the way I had structed the jsx and html would not work. I was getting a lot of layout
    breaks. Hence I went back, reviewed some Tailwind 4 examples and templates.
    Applied the approach of more nested divs, but offloading a lot of styling on the
    subsequent layers. I was trying to get too much 'mileage' out of my divs and containers.
    This refactor cycle and approach I also worked in each element top down, making sure mobile worked
    first then desktop. This helped to add the nesting divs where each div was more focused on it purpose.
    It may seem verbose, but I understand now why tailwind creates most of their template this way.
    Also reminded myself of flex-auto! Very helpful in getting the hang of layouts again.

--- src/app/components/molecules/EmailForm.tsx
+++ src/app/components/molecules/EmailForm.tsx
@@ -34,17 +34,12 @@ export default function EmailForm() {
   };
 
   return (
-    <form
-      onSubmit={onSubmit}
-      noValidate
-      className="mt-4 flex flex-col items-center"
-    >
-      <h2>Subscribe and get notified</h2>
-      <div className="flex w-full flex-col items-center md:mt-10 md:flex-row md:items-start md:gap-4">
-        <label htmlFor="email" className="sr-only">
-          Email Address:
-        </label>
-        <div className="w-full max-w-[17.625rem] md:max-w-full">
+    <form onSubmit={onSubmit} className="flex flex-col" noValidate>
+      <h2 className="text-neutralDarkBlue mt-4 text-center text-xs sm:text-xl">
+        Subscribe and get notified
+      </h2>
+      <div className="mt-8 flex flex-auto flex-col gap-2.5 sm:flex-row sm:items-start sm:gap-4">
+        <div className="mx-2.5 flex-auto sm:mx-0">
           <input
             type="email"
             name="email"
@@ -52,27 +47,20 @@ export default function EmailForm() {
             placeholder="Your email address…"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className={`${error ? "border-secondaryRed" : ""} mt-8 w-full md:mt-0 md:h-14`}
+            className={
+              '${error ? "border-secondaryRed" : ""} text-inputText border-secondaryBlue md:text-base` h-10 w-full rounded-full border bg-white pl-8 text-xs sm:h-14 sm:w-full'
+            }
             aria-invalid={!!error}
             aria-describedby={error ? "email-error" : undefined}
             ref={emailRef}
-            required
           />
           {error && (
-            <p
-              role="alert"
-              id="email-error"
-              className="text-secondaryRed mt-[2px] justify-start text-center text-[0.625rem] leading-tight font-normal tracking-tight italic md:mt-4 md:pl-8 md:text-left"
-            >
+            <p className="text-secondaryRed mt-0.5 mb-2.5 pl-0 text-center text-[0.625rem] leading-tight tracking-tight italic sm:mt-1.5 sm:pl-8 sm:text-left sm:text-xs">
               {error}
             </p>
           )}
         </div>
-
-        <button
-          className="mt-2.5 h-10 w-full max-w-[17.625rem] rounded-full md:mt-0 md:h-14 md:w-[12.5rem]"
-          type="submit"
-        >
+        <button className="bg-primaryBlue hover:bg-primaryBlueHover mx-2.5 h-10 min-w-52 cursor-pointer rounded-full text-xs font-semibold text-white shadow-[0px_5px_10px_2px_rgba(76,123,243,0.23)] transition sm:mx-0 sm:h-14 sm:text-base">
           Notify Me
         </button>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,37 +7,43 @@ import EmailForm from "@/molecules/EmailForm";
 
 export default function Home() {
   return (
-    <main className="flex h-full min-h-dvh w-full flex-col items-center px-7 pt-20 pb-9">
-      <Logo className="h-[1.5rem] w-[3.5rem] md:h-[2.5rem] md:w-[5.625rem]" />
-      <section className="mt-8 flex flex-col justify-center md:mt-10">
-        <h1>
-          We are launching <span>soon!</span>
-        </h1>
-        <EmailForm />
-        <Image
-          src="/illustration-dashboard.png"
-          alt=""
-          width={1280}
-          height={720}
-          className="mt-[4.5rem] md:mt-[4.25rem] md:max-w-[40rem]"
-          aria-hidden="true"
-          role="presentation"
-          priority
-        />
-      </section>
-
-      <nav
-        aria-label="Social links"
-        className="mt-[7.5rem] flex flex-row gap-3 md:mt-[4.5rem]"
-      >
-        <FacebookIcon />
-        <TwitterIcon />
-        <InstagramIcon />
-      </nav>
-
-      <footer className="text-neutralGray mt-7 text-[0.625rem] leading-normal font-light">
-        &copy; Copyright Ping. All rights reserved.
-      </footer>
-    </main>
+    <div className="flex min-h-dvh w-full">
+      <div className="flex flex-auto flex-col items-center px-7 pt-20 pb-9 sm:pb-10">
+        <header>
+          <Logo className="h-6 w-14 sm:h-10 sm:w-24" />
+        </header>
+        <main>
+          <section className="mt-8 sm:mt-10">
+            <h1 className="text-neutralGray text-center text-[1.375rem]/[2rem] font-light sm:text-[3rem]/[3.75rem]">
+              We are launching{" "}
+              <span className="text-neutralDarkBlue text-center font-bold">
+                soon!
+              </span>
+            </h1>
+            <EmailForm />
+            <Image
+              src="/illustration-dashboard.png"
+              alt=""
+              width={1280}
+              height={720}
+              className="mt-[4.5rem] w-[40rem] sm:mt-24"
+              aria-hidden="true"
+              role="presentation"
+              priority
+            />
+          </section>
+        </main>
+        <footer className="mt-[7.5rem] flex flex-auto flex-col items-center justify-end gap-6 sm:mt-[4.5rem]">
+          <div className="flex flex-row gap-3">
+            <FacebookIcon />
+            <TwitterIcon />
+            <InstagramIcon />
+          </div>
+          <p className="text-neutralGray text-xs">
+            &copy; Copyright Ping. All rights reserved.
+          </p>
+        </footer>
+      </div>
+    </div>
   );
 }
